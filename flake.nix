0ee54543a38e6aec467cebbbd233ab9c6abbfed7
Mikael Hugo <mikkihugo@users.noreply.github.com>
2025-12-05 17:49:49 +0100

    Update to OTP 28: nixos-25.11 and version 0.1.0-beta12

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Chunker development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
